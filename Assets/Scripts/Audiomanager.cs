3bff961170f6abc217ae00bb5579ac48b5c165b1
canoezbal@gmail.com <canoezbal@gmail.com>
2026-05-20 15:12:38 +0200

    main menu music, background music, bgm and every sfx sound added and they are documented here https://www.notion.so/36445a37e3938032adc1e82dff8ee54d?v=36445a37e39380e3b539000c999a2d55

--- Assets/Scripts/Audiomanager.cs
+++ Assets/Scripts/Audiomanager.cs
@@ -1,63 +1,90 @@
 using UnityEngine;
 using Random = UnityEngine.Random;
 
+namespace Manager
+{
     public class Audiomanager : MonoBehaviour
-    {   
+    {
+        public static Audiomanager Instance { get; private set; }
+
         [SerializeField] public AudioSource menuMusic;
         [SerializeField] public AudioSource[] bgm;
-        public AudioSource[] sfx;
+        [SerializeField] public AudioSource[] sfx;
 
         private int _currentBGM;
         private bool _playingBGM;
-        
-        ////////////////////////////////////////////////////////////////////////////////////////////////
+
+        #region Lifecycle
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
         private void Update()
         {
             IsBGMPlaying();
         }
-        ////////////////////////////////////////////////////////////////////////////////////////////////
-        public void PlaymainMenu()
+
+        #endregion
+
+        #region Music
+
+        public void PlayMainMenu()
         {
             StopMusic();
             menuMusic.Play();
         }
-        ////////////////////////////////////////////////////////////////////////////////////////////////
+
         public void PlayBGM()
         {
             StopMusic();
             _currentBGM = Random.Range(0, bgm.Length);
             bgm[_currentBGM].Play();
             _playingBGM = true;
-        }        
-        ////////////////////////////////////////////////////////////////////////////////////////////////
-        private void IsBGMPlaying()
-        {
-            if (!_playingBGM) return;
-            // Wenn die Musik nicht mehr spielt, gehe zum nächsten element
-            if (bgm[_currentBGM].isPlaying) return;
-            _currentBGM++;
-            // ist der Array durschgelaufen fange von vorne an
-            if (_currentBGM >= bgm.Length)
-            {
-                _currentBGM = 0;
-            }
-            bgm[_currentBGM].Play();
         }
-        ////////////////////////////////////////////////////////////////////////////////////////////////
-        
+
         public void StopMusic()
         {
             menuMusic.Stop();
-          
+
             foreach (var track in bgm)
-            {
                 track.Stop();
-            }
+
             _playingBGM = false;
         }
-        ////////////////////////////////////////////////////////////////////////////////////////////////
+
+        private void IsBGMPlaying()
+        {
+            if (!_playingBGM) return;
+            if (bgm[_currentBGM].isPlaying) return;
+
+            // Wenn die Musik nicht mehr spielt, gehe zum nächsten Element
+            _currentBGM++;
+
+            // Ist der Array durchgelaufen, fange von vorne an
+            if (_currentBGM >= bgm.Length)
+                _currentBGM = 0;
+
+            bgm[_currentBGM].Play();
+        }
+
+        #endregion
+
+        #region SFX
+
         public void PlaySfx(int sfxToPlay)
         {
             sfx[sfxToPlay].PlayOneShot(sfx[sfxToPlay].clip);
         }
-} 
+
+        #endregion
+    }
+}
